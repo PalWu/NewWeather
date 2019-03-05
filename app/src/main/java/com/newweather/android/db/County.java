c4730beb2c70504665e9bd7afb3108ad19c8185b
wyt <1830186867@qq.com>
2019-03-05 17:12:39 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/newweather/android/db/County.java
@@ -0,0 +1,42 @@
+package com.newweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private String cityID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(String cityID) {
+        this.cityID = cityID;
+    }
+}
